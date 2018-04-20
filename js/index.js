f74d11956015b5d14c78adde681038c194d522d8
荆文征 <200739491@qq.com>
2018-04-20 14:09:34 +0800

    引入 所需要的文件

--- js/index.js
+++ js/index.js
@@ -1,14 +1,10 @@
 $(function() {
 
-
   var { Query, User } = AV;
   AV.init("Nzr6uILxRVGe2jgpykgH1fyg-gzGzoHsz", "nTBkk57i7SYrn2OUEP1mn78Y");
 
-  // Set the command-line prompt to include the user's IP Address
-  // $('.prompt').html('[' + codehelper_ip["IP"] + '@HTML5] # ');
   $('.prompt').html('[loading user...@HTML5] # ');
 
-  // Initialize a new terminal object
   var term = new Terminal('#input-line .cmdline', '#container output');
 
   const terminaluser = new TerminalUser(term);
